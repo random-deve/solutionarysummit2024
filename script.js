a6f289dccfcd56f13d96060cc89982acbad590f0
frfr <chookitypok123@gmail.com>
2023-11-20 14:56:21 +0100

    e

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ function toggleResponsive() {
   var x = document.getElementById("topNav");
   if (x.className === "topnav") {
     x.className += " responsive";
-    document.getElementById("topNav").style = "height: 375px;";
+    x.style = "height: 375px;";
   } else {
     x.className = "topnav";
-    document.getElementById("topNav").style = "height: 60px;";
+    x.style = "height: 60px;";
   }
 }
 
 document.addEventListener("scroll", () => {
   var x = document.getElementById("topNav");
   if (document.body.scrollTop >= 20) {
-    document.getElementById("topNav").style = "height: 60px;";
+    x.style = "height: 60px;";
+    document.getElementById("backgroundimg").style = "height: 0px;"
   } else {
-    document.getElementById("topNav").style = "height: 250px;";
+    x.style = "height: 250px;";
+    document.getElementById("backgroundimg").style = "height: 250px;"
   }
 });
 
